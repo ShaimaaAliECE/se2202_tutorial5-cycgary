e9ae9a61399e271475bceabf601f629130ea1454
Yichuan Cao <garycyctj2@gmail.com>
2021-10-19 21:37:44 -0400

    ycao453

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the game turns
 
 //initialize the game
-
+let nextLabel = document.getElementById('next-lbl');
+nextLabel.innerText = " " + nextPlayer;
 // use the value stored in the nextPlayer variable to indicate who the next player is
 
 
@@ -11,7 +12,12 @@ createGameBoard()
 function createGameBoard()
 {
     // Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
-   
+    let gameBoard = document.getElementsByTagName('td');
+    for (count = 0; count < gameBoard.length; count++) {
+        let button1 = document.createElement('button');
+        button1.innerText = "[ ]";
+        gameBoard[count].appendChild(button1);
+    }
 }
 
 // Programatically add 'takeCell' as an event listener to all the buttons on the board
@@ -24,6 +30,16 @@ for (let i=0; i<btns.length; i++)
 // This function will be used to respond to a click event on any of the board buttons.
 function takeCell(event)
 {
+    let button2 = event.target;
+    button2.innerText = "[" + nextPlayer + "]";
+    button2.disabled = true;
+    if (nextPlayer == 'X') {
+        nextPlayer = 'O';
+    } 
+    else if (nextPlayer == 'O') {
+        nextPlayer = 'X';
+    }
+    nextLabel.innerText = " " + nextPlayer;
     /*
         When the button is clicked, the space inside its square brackets is replaced by the value in the nextPlayer before switching it
     */
@@ -34,6 +50,11 @@ function takeCell(event)
     if (isGameOver())
     {
         // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
+        let h1 = document.createElement("h1");
+        let words = document.createTextNode("Game Over!");
+        h1.appendChild(words);
+        let gameOver = document.getElementById('game-over-lbl');
+        gameOver.appendChild(h1);
     }
 
     // I'll leave declaring the winner for your intrinsic motivation, it's not required for this assignment 
@@ -42,5 +63,14 @@ function takeCell(event)
 function isGameOver()
 {
     // This function returns true if all the buttons are disabled and false otherwise 
-   
+    let count = 0;
+    for (let i = 0; i < btns.length; i++) {
+        if (btns[count].disabled == true) {
+            count++;
+        }
+    }
+    if (count == btns.length) {
+        return true;
+    }
+    return false;
 }
